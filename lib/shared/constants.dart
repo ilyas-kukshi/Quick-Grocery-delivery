74d5d2e30c5bf3cac72c1b57e517ff874bbd9e9b
ilyas-kukshi <ilyaskukshiwala53@gmail.com>
2022-04-29 07:18:35 +0530

    api key change

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,8 +1,9 @@
 class Constants {
   static String url =
       'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=';
-  static String mapApiKey = "&key=AIzaSyDATAVetxk1sEQG5483SqMHLMsjf9iFtfE";
-  static String onlyMapApiKey = "AIzaSyDATAVetxk1sEQG5483SqMHLMsjf9iFtfE";
+  static String mapApiKey = "&key=AIzaSyCQO-86E-98niNV4q7ofpWB0pbk4xehXsw";
+// AIzaSyDATAVetxk1sEQG5483SqMHLMsjf9iFtfE
+  static String onlyMapApiKey = "AIzaSyCQO-86E-98niNV4q7ofpWB0pbk4xehXsw";
   static String geoCodeType = '&types=geocode';
   static String language = "&language=en";
 
